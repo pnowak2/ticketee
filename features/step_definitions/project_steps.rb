36146c5316750a670f0b397d00fb687f79aa662b
Piotr Nowak <p.nowak2@gmail.com>
2012-02-14 14:36:00 +0100

    Creation of projects feature complete

---
+++ features/step_definitions/project_steps.rb
@@ -0,0 +1,23 @@
+Given /^I am on the homepage$/ do
+  visit "/"
+end
+
+When /^I follow "([^"]*)"$/ do |link|
+  click_link link
+end
+
+When /^I fill in "([^"]*)" with "([^"]*)"$/ do |field, value|
+  fill_in field, :with => value
+end
+
+When /^I press "([^"]*)"$/ do |button|
+  click_button(button)
+end
+
+Then /^I should see "([^"]*)"$/ do |content|
+  page.should have_content(content)
+end
+
+Then /^I should be on the project page for "([^"]*)"$/ do |name|
+  project_path(Project.find_by_name!(name))
+end
