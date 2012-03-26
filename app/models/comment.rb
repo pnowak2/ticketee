c7af267a7fc5bbb3220b071e90be43ed981fa6a6
Piotr Nowak <p.nowak2@gmail.com>
2012-03-26 15:00:06 +0200

    Add button so users can toggle watching on a ticket

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,6 +10,8 @@ class Comment < ActiveRecord::Base
   belongs_to :state
   belongs_to :previous_state, :class_name => "State"
 
+  after_create :creator_watches_ticket
+
   private
     def set_ticket_state
       self.ticket.state = self.state
@@ -19,4 +21,10 @@ class Comment < ActiveRecord::Base
     def set_previous_state
       self.previous_state = ticket.state
     end
+
+    def creator_watches_ticket
+      ticket.watchers << user
+    end
+
+
 end
