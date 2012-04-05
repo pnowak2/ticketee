19145c2ad398406a2ef700d50e5b7dd44bd64737
Piotr Nowak <p.nowak2@gmail.com>
2012-04-05 10:20:47 +0200

    Support XML & JSON with /api/v1/projects

--- spec/apis/v1/projects_spec.rb
+++ spec/apis/v1/projects_spec.rb
@@ -35,5 +35,12 @@ describe "/api/v1/projects/", :type => :api do
       end.should be_false
     end
 
+    it "xml" do
+      get "#{url}.xml", :token => token
+      last_response.body.should eql(Project.readable_by(user).to_xml)
+      projects = Nokogiri::XML(last_response.body)
+      projects.css("project name").text.should eql("Ticketee")
+    end
+
   end
 end
